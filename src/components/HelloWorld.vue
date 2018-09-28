ca4cfb38334ee28b0266718ba93cd5d6e2671709
LWD8 <948929032@qq.com>
2018-09-28 15:32:13 +0800

    px2rem响应式

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="hello">
+    <div class="top_box">
+      <div class="text_item">印象金币</div>
+      <div class="text_item">印象股</div>
+    </div>
+    <ul>
+      <li v-for="i in 10" :key="i" :style="{width: `${i}rem`, backgroundColor: 'red',height:'30px'}"></li>
+    </ul>
+    <p class="aa">迪迪迪迪</p>
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+//px转换rem
+.top_box{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 106px;
+  line-height: 106px;
+  .text_item{
+    flex:1;
+    text-align: center;
+    font-size: 32px;
+  }
+}
+.aa{
+  font-size: 90px;
+}
+</style>
