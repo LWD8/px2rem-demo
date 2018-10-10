650c3be603f1f7351c37d8205ecba0f740ed6d4f
LWD8 <948929032@qq.com>
2018-10-10 18:26:53 +0800

    vue-loader配置autoprefixer、postcss-plugin-px2rem

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -18,5 +18,16 @@ module.exports = {
     source: 'src',
     img: 'src',
     image: 'xlink:href'
-  }
+  },
+  postcss: [
+    require('autoprefixer')({
+      browsers: ['iOS >= 7', 'Android >= 4.1']
+    }),
+    require('postcss-plugin-px2rem')({
+      rootValue: 36, // 这里对应的是750的设计图尺寸
+      selectorBlackList: [],
+      mediaQuery: true,
+      propBlackList: [] // 如果要保持font-size不转换，替换为 ['font-size']
+    })
+  ]
 }
